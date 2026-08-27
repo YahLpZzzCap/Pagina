001ff0e2c6c33cc8fd9a539770526f6a1641705e
AndyX67 <gaddtv2180@gmail.com>
2026-08-26 19:29:49 -0600

    Add files via upload

---
+++ Reseñas.js
@@ -0,0 +1,80 @@
+let resenas = JSON.parse(localStorage.getItem("resenas")) || [];
+
+
+function agregarResena() {
+
+    let nombre = document.getElementById("nombre").value;
+    let estrellas = document.getElementById("estrellas").value;
+    let comentario = document.getElementById("comentario").value;
+
+
+    if (nombre.trim() === "" || comentario.trim() === "") {
+
+        alert("Completa todos los campos");
+
+        return;
+    }
+
+
+    let nuevaResena = {
+
+        nombre: nombre,
+        estrellas: estrellas,
+        comentario: comentario
+
+    };
+
+
+    resenas.push(nuevaResena);
+
+
+    localStorage.setItem(
+        "resenas",
+        JSON.stringify(resenas)
+    );
+
+
+    document.getElementById("nombre").value = "";
+
+    document.getElementById("comentario").value = "";
+
+
+    mostrarResenas();
+
+}
+
+
+function mostrarResenas() {
+
+    let lista = document.getElementById("listaResenas");
+
+    lista.innerHTML = "";
+
+
+    resenas.forEach(function(resena) {
+
+        let estrellas = "⭐".repeat(
+            Number(resena.estrellas)
+        );
+
+
+        lista.innerHTML += `
+
+            <div class="resena">
+
+                <h3>${resena.nombre}</h3>
+
+                <p>${estrellas}</p>
+
+                <p>${resena.comentario}</p>
+
+            </div>
+
+        `;
+
+    });
+
+}
+
+
+mostrarResenas();
